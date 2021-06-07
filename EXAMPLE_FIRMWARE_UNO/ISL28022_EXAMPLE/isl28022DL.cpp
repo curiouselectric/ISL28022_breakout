4bb29d3aecc17c188b8bd5e2f9b72a174c812101
Matthew Little <matt@re-innovation.co.uk>
2021-06-07 11:12:38 +0100

    updated instructions

--- EXAMPLE_FIRMWARE_UNO/ISL28022_EXAMPLE/isl28022DL.cpp
+++ EXAMPLE_FIRMWARE_UNO/ISL28022_EXAMPLE/isl28022DL.cpp
@@ -29,6 +29,7 @@ void power_isl28022::begin(int _address)
 
   // Set up everything:
   // RST/BRNG1/BRNG0/PG1/PG0/BADC3/BADC2/BADC1/BADC0/SADC3/SADC2/SADC1/SADC0/MODE2/MODE1/MODE0
+  // Shunt with 320mV range so need to set PG1 = 1 and PG0 = 1 (from datasheet)
   // Set it to:
   // 0b 0 11 11 0011 0011 111
   Wire.write (0b01111001);
@@ -37,18 +38,18 @@ void power_isl28022::begin(int _address)
 
   // Set up the Calibration Register
   // This allows the Power and Current registers to be used
-  Wire.beginTransmission(_address);  // Start write to address 0b1000000
-  Wire.write(0x05);    // Write to the calibration register
+  Wire.beginTransmission(_address);  // Start write to IC address 
+  Wire.write(0x05);                 // Write to the calibration register
 
   // OLD values were:
   // 4474 = 117A in HEX
   // Binary: 0001 0001 0111 1010
 
-  // NEW value is:
   // 5mOhm Shunt with 320mV range
   // Calibration register needs to be set to 4194 (dec) = 1062 (hex)
   Wire.write (0x10);
   Wire.write (0x62);
+
   Wire.endTransmission();
 }
 
@@ -75,14 +76,14 @@ float power_isl28022::readRegister(int _address, int _reg)
 
   if (_reg == SHUNTVOLTREG)
   {
-    // ****THIS DEPENDS UPON THE SADC BITS*******
-    // ****RANGE = 11 (full scale up to +/-320mV)
     Serial.print(F("SV: "));
+    // ****THIS DEPENDS UPON THE SADC BITS*******
+    // ****RANGE = 11 (full scale up to +/-160mV)
     // The bus voltage is a 15 bit value which needs to be converter.
     // Bit 16 is a sign bit (2's complement?)
     // Need to add them together, with the correct weighting factor (256)
     valueInt16 = firstByte * 256 + secondByte; // Convert to a value
-    // Each bit is (320mV / 2^15)
+    // Each bit is (160mV / 2^15)
     // So value * 0.00001V = shunt voltage
     // This is too small, so keep number in milli Volts
     valueFloat = ((valueInt16 * 320.0) / 32768.0);
@@ -103,16 +104,15 @@ float power_isl28022::readRegister(int _address, int _reg)
   }
   else if (_reg == POWERREG)
   {
+    Serial.print(F("P: "));
     // Only has meaning if calibration register (0x05) is programmed.
     valueInt16 = firstByte * 256 + secondByte; // Convert to a value
-
-    Serial.print(F("P: "));
     // From ISL28022 datasheet
     // Actual Power = reading * Power LSB * 5000 * 2
     // Power LSB = Current LSB x Voltage LSB
-    // Power LSB = 1.953125 mA x 4 mV = 7.8125 uW per LSB 
+    // Power LSB = 1.953125 mA x 4 mV = 7.8125 uW per LSB
     // 1 LSB = 7.8125uW
-    valueFloat = (valueInt16 * (7.8125) * 5000.0 * 2.0) / 1000000.0;
+    valueFloat = (valueInt16 * 7.8125 * 5000.0 * 2.0) / 1000000.0;
   }
   else if (_reg == CURRENTREG)
   {
@@ -120,7 +120,7 @@ float power_isl28022::readRegister(int _address, int _reg)
     // Only has meaning if calibration register (0x05) is programmed.
     valueInt16 = firstByte * 256 + secondByte; // Convert to a value
     // From ISL28022 datasheet
-    // Actual current = ((reading / 2^15) * Max Shunt Voltage) / Shunt resistance
+    // Actual current = ((reading / 2^15) * Max Shunt Voltage (mV)) / Shunt resistance(mOhm)
     valueFloat = ((valueInt16 / 32768.0) * 320.0) / 5.0 ;
   }
   Serial.print(valueFloat, 2);
